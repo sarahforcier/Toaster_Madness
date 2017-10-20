0a1e0b5099527e3bf5fdba2a4c38c7eb39fe9cb6
Sarah Forcier <sforcier@seas.upenn.edu>
2017-10-20 15:43:08 -0400

    starter unreal

---
+++ Source/ToasterMadness/ToasterMadnessGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ToasterMadnessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOASTERMADNESS_API AToasterMadnessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
